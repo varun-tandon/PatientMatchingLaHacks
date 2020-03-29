25c378a461badee5b88180a225dc0673385ee435
vincent912 <2007vincentli@gmail.com>
2020-03-29 02:08:19 -0700

    added functionality for ensemble voting method

--- submission.py
+++ submission.py
@@ -3,15 +3,20 @@ from evaluation import *
 from confidence_aggregation import *
 from levenshtein_distance_stats import *
 from clustering import *
+from voting import *
+from hashing import *
 import fuzzy
 soundex = fuzzy.Soundex(4)
 FILENAME = 'Patient Matching Data.csv'
 
 df_patient = load_and_clean_data(FILENAME)
 
-matrix = generate_confidence_matrix(compare_patients, df_patient, 50)
+create_hash_tokens(df_patient)
+
+#matrix = generate_confidence_matrix(compare_patients, df_patient, 50, df_patient.shape[0])
+matrix = generate_ensemble_votes(df_patient)
 n_components, labels = calculate_connected_components(matrix)
-tp, fp, tn, fn = calculate_unbiased_confusion(labels, df_patient)
+tp, fp, tn, fn = calculate_confusion_matrix(labels, df_patient)
 
 print('Accuracy: {}, Precision: {}, Recall: {}, F1: {}'.format(
     calculate_accuracy(tp, fp, tn, fn),
